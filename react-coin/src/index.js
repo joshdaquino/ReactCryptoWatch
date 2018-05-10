5faa7aaa774f6f5cbf0ca11babfb05626671b3ef
joshdaquino <josh@joshaquino.com>
2018-05-10 11:48:55 +0900

    Rendering React components

--- react-coin/src/index.js
+++ react-coin/src/index.js
@@ -1,1 +1,11 @@
-alert('Hi');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <h1> React Coin</h1>;
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
