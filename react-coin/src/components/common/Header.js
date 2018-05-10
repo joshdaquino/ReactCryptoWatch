cc03d023734c0f93c7661a6856eddb84741616e7
joshdaquino <josh@joshaquino.com>
2018-05-10 23:19:09 +0900

    Search Form

--- react-coin/src/components/common/Header.js
+++ react-coin/src/components/common/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.png';
+import Search from './Search';
 import { Link } from "react-router-dom";
 import './Header.css';
 
@@ -9,6 +10,7 @@ const Header = () => {
       <Link to="/">
         <img src={logo} alt="logo" className="Header-logo" />
       </Link>
+      <Search />
     </div>
   );
 }
